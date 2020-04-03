be18f2099ab67b1abb18d9ddc35bec228d074da4
baob <coder@onesandthrees.com>
2020-04-03 21:44:47 +0100

    add BOT_TOKEN - but error: not_allowed_token_type

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
+const bot = new SlackBot({
+	token: `${process.env.BOT_TOKEN}`,
+	name: 'inspirenuggets',
+});
